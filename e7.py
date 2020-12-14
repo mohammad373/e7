f43008be9a4e6a2006e772462ac0ffd6dd46c526
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-14 13:35:05 +0330

    Update and rename README.md to e7.py

---
+++ e7.py
@@ -0,0 +1,61 @@
+# e7
+
+import os
+import time
+import socket
+import sys
+from colorama import Fore
+
+def __target1__():
+    os.system("clear")
+    time.sleep(0.5)
+    print(Fore.RED +"""
+        ///////////////////////
+        //////////////////////
+        /////////////////////
+        ////////////////////
+        ///////////////////
+        //////////////////
+        /////////////////
+        ////////////////
+        ///////////////
+        //////////////
+        /////////////
+        ////////////
+        ///////////
+        //////////
+        /////////
+        ////////
+        ///////
+        //////
+        /////
+        ////
+        ///
+        //
+        /""")
+    target = input(Fore.BLUE + "\n[!]" + Fore.RED + " ~ " + Fore.YELLOW + "Pleass Enter Your Address Target" + Fore.GREEN + " ==>  ")
+    if target == "" or None:
+        try:
+            time.sleep(0.5)
+            print(Fore.RED + "\nError : Your Target Is None Or Not Found ;(")
+            time.sleep(1)
+            sys.exit()
+        except:
+            pass
+    if not "http" in target or not "https" in target :
+        target = "http://" + target
+    ip = socket.gethostbyname(target)
+    while True:
+        time.sleep(0.1)
+        coun = 1
+        r = requests.get(target)
+        if r.status_code == 404 or r.status_code == 500:
+            print(Fore.YELLOW + "[ " + Fore.RED + coun+ Fore.YELLOW + " ]" + Fore.BLUE + " ~ " + Fore.RED + target + Fore.YELLOW + ip)
+            coun += 1
+        if r.status_code == 200:
+            print(Fore.BLUE + "[ " + Fore.GREEN + coun+Fore.BLUE + " ]" + Fore.YELLOW + " ~ " + Fore.REEN + target + Fore.BLUE + ip)
+            coun += 1
+        if coun == 150:
+            time.sleep(1)
+            sys.exit()
+__target1__()
